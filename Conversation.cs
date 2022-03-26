f64c0c56b3d4637766d4f636344d7d9996a3eff6
世元 李 <1498045907@qq.com>
2022-03-26 21:30:56 +0800

    Init

--- Conversation.cs
+++ Conversation.cs
@@ -13,148 +13,99 @@ using System.Windows;
 
 namespace DrawAndGuess
 {
-    internal class Conversation
+    internal class Conversation : ConversationBase
     {
-        protected byte[] RecvedData = null;
+        readonly static string IP = "119.49.86.85";
+        readonly static short Port = 25535;
+
         public delegate void OnBitmapHandler(Bitmap Data);
-        readonly byte[] OKReply = new byte[2] { 122, 122 };
-        readonly string IP = "192.168.3.138";
-        readonly short Port = 25535;
-        readonly TcpClient Client;
-        readonly NetworkStream StreamL;
+
         public bool IsMainHost { get; private set; }
-        public event Action OnHosted;
-        public event Action UnHosted;
-        public event OnBitmapHandler OnBitmap;
-
-        protected ManualResetEvent MsgRecved = new ManualResetEvent(false);
-
-        byte[] RecvInsertRange(NetworkStream Stream, int Size)
-        {
-            var  RecvedData = new byte[Size];
-            int RecvedSize = 0;
-
-            do
-            {
-                RecvedSize += Stream.Read(RecvedData, RecvedSize, (int)(Size - RecvedSize));
-            } while (RecvedSize < Size);
-            return RecvedData;
-        }
-
-        void PaassiveModole(object ?Param)
+        public event Action? OnHosted;
+        public event Action ?UnHosted;
+        public event OnBitmapHandler ?OnBitmap;
+        void PaassiveModole(object? Param)
         {
             NetworkStream Stream = Client.GetStream();
             while (true)
             {
-                 try
-                 {
+                try
+                {
                     byte[] HeadData = RecvInsertRange(Stream, Marshal.SizeOf(typeof(DataHead)));
 
                     DataHead Head = (DataHead)BytesToStuct(HeadData, typeof(DataHead));
                     if (Head.Rev != 255)
-                    {
-                         MsgRecved.Set(); continue; 
-                    }
+                        continue;
                     ConversationType ct = (ConversationType)Head.ct;
                     CommandType cm = (CommandType)Head.cmt;
 
-                    if (ct == ConversationType.ServerCommand)
+                    switch(ct)
                     {
-                        if (cm == CommandType.Test)
-                        {
-                            byte[] Buf = RecvInsertRange(Stream, (int)Head.Length);
-
-                            MessageBox.Show(Encoding.ASCII.GetString(Buf));
-                        }
-
-                        if (cm == CommandType.SetName)
-                        {
-                            byte[] Buf = RecvInsertRange(Stream, (int)Head.Length);
-                            if (Buf[0] == 1)
+                        case ConversationType.ServerCommand:
+                            switch(cm)
                             {
-                                IsMainHost = true;
-                                OnHosted?.Invoke();
+                                case CommandType.Test:
+                                    {
+                                        byte[] Buf = RecvInsertRange(Stream, (int)Head.Length);
+                                        MessageBox.Show(Encoding.ASCII.GetString(Buf));
+                                        break;
+                                    }
+                                case CommandType.SetName:
+                                    {
+                                        byte[] Buf = RecvInsertRange(Stream, (int)Head.Length);
+                                        if (Buf[0] == 1)
+                                        {
+                                            IsMainHost = true;
+                                            OnHosted?.Invoke();
+                                        }
+                                        else
+                                        {
+                                            IsMainHost = false;
+                                            UnHosted?.Invoke();
+                                        }
+                                        break;
+                                    }
                             }
-                            else
+                            break;
+                        case ConversationType.ServerData:
+                            switch(cm)
                             {
-                                IsMainHost = false;
-                                UnHosted?.Invoke();
+                                case CommandType.Test:
+                                    byte[] Buf = RecvInsertRange(Stream, (int)Head.Length);
+                                    break;
+                                case CommandType.BeginData:
+                                    {
+                                        var RecvedData = RecvInsertRange(Stream, (int)Head.Length);
+
+                                        if (RecvedData != null && OnBitmap != null)
+                                        {
+                                            try
+                                            {
+                                                var Bp = ImagingMedu.GetBitmap(RecvedData);
+                                                OnBitmap?.Invoke(Bp);
+                                            }
+                                            catch (Exception) { }
+                                        }
+                                        break;
+                                    }
                             }
-                        }
+                            break;
+                        default:
+                            break;
                     }
-                    else if (ct == ConversationType.ServerData)
-                    {
-                        if (cm == CommandType.Test)
-                        {
-                            byte[] Buf = RecvInsertRange(Stream, (int)Head.Length);
-                        }
-                        else if (cm == CommandType.BeginData)
-                        {
-                            RecvedData = RecvInsertRange(Stream, (int)Head.Length);
-
-                            if (RecvedData != null && OnBitmap != null)
-                            {
-                                try
-                                {
-                                    var Bp = ImagingMedu.GetBitmap(RecvedData);
-
-                                    OnBitmap?.Invoke(Bp);
-                                }
-                                catch (Exception)
-                                {
-
-                                }
-                            }
-
-                        }
-                }
                 }
                 catch (Exception ex)
                 {
                     MessageBox.Show(ex.Message);
                     break;
                 }
-                MsgRecved.Set();
             }
 
         }
-        public enum ConversationType
-        {
-            ServerCommand=0,
-            ServerData=1,
-            ClientCommand=2,
-            ClientData=3
-        }
 
-        public enum CommandType
+        public Conversation() : base(IP, Port)
         {
-            SetName=0,
-            BeginData=1,
-            EndData=2,
-            Test=3
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct DataHead
-        {
-            public uint Rev;
-            public byte ct;
-            public byte cmt;
-            public uint Length;
-        }
-
-        public Conversation()
-        {
-            Client = new TcpClient();
-            Client.Connect(IP, Port);
-            StreamL = Client.GetStream();
-            MsgRecved.Reset();
             new Thread(new ParameterizedThreadStart(PaassiveModole)) { IsBackground = true }.Start();
-        }
-        void WaitForMsg()
-        {
-             MsgRecved.WaitOne();
-            MsgRecved.Reset();
         }
         public void Test()
         {
@@ -165,7 +116,7 @@ namespace DrawAndGuess
         }
         public void PublishImg(byte [] Data)
         {
-            TransHead(ConversationType.ClientCommand, CommandType.EndData, (uint)Data.Length);
+            TransHead(ConversationType.ClientData, CommandType.EndData, (uint)Data.Length);
             StreamL.Write(Data);
         }
         public void SetName(string Name)
@@ -175,64 +126,6 @@ namespace DrawAndGuess
 
             StreamL.Write(Data);
         }
-        #region Area
-        public void TransHead(ConversationType ct, CommandType cm, uint Length)
-        {
-            DataHead Dt = new DataHead() { ct = (byte)ct, cmt = (byte)cm, Rev = 0xff, Length = Length };
-            var Buffer = StructToBytes(Dt);
-
-            StreamL.Write(Buffer, 0, Buffer.Length);
-        }
-        //// <summary>
-        /// 结构体转byte数组
-        /// </summary>
-        /// <param name="structObj">要转换的结构体</param>
-        /// <returns>转换后的byte数组</returns>
-        public static byte[] StructToBytes(object structObj)
-        {
-            //得到结构体的大小
-            int size = Marshal.SizeOf(structObj);
-            //创建byte数组
-            byte[] bytes = new byte[size];
-            //分配结构体大小的内存空间
-            IntPtr structPtr = Marshal.AllocHGlobal(size);
-            //将结构体拷到分配好的内存空间
-            Marshal.StructureToPtr(structObj, structPtr, false);
-            //从内存空间拷到byte数组
-            Marshal.Copy(structPtr, bytes, 0, size);
-            //释放内存空间
-            Marshal.FreeHGlobal(structPtr);
-            //返回byte数组
-            return bytes;
-        }
-
-        /// <summary>
-        /// byte数组转结构体
-        /// </summary>
-        /// <param name="bytes">byte数组</param>
-        /// <param name="type">结构体类型</param>
-        /// <returns>转换后的结构体</returns>
-        public static object BytesToStuct(byte[] bytes, Type type)
-        {
-            //得到结构体的大小
-            int size = Marshal.SizeOf(type);
-            //byte数组长度小于结构体的大小
-            if (size > bytes.Length)
-            {
-                //返回空
-                return null;
-            }
-            //分配结构体大小的内存空间
-            IntPtr structPtr = Marshal.AllocHGlobal(size);
-            //将byte数组拷到分配好的内存空间
-            Marshal.Copy(bytes, 0, structPtr, size);
-            //将内存空间转换为目标结构体
-            object obj = Marshal.PtrToStructure(structPtr, type);
-            //释放内存空间
-            Marshal.FreeHGlobal(structPtr);
-            //返回结构体
-            return obj;
-        }
-        #endregion
+       
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace DrawAndGuess
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly Conversation CS;
+        private readonly Conversation Server;
         private readonly Bitmap Buffer;
         private readonly WriteableBitmap BackSide;
         private readonly Graphics Pen;
@@ -34,7 +34,7 @@ namespace DrawAndGuess
         private SpinLock @lock = new SpinLock();
         private bool LockFlag = false;
 
-        private byte[] PublicData;
+        private byte[] ?PublicData;
         void Flush(PaintEventHandle e)
         {
             BackSide.Lock();
@@ -47,16 +47,16 @@ namespace DrawAndGuess
         void ImgMutil(bool io, PaintEventHandle e, ref byte[] Opt)
         {
             LockFlag = false;
-            @lock.Enter(ref LockFlag);
+      //      @lock.Enter(ref LockFlag);
             if (io)
             {
                 Flush(e);
             }
             else
             {
-                Opt = Buffer.CompressionImage(35);
+                Opt = Buffer.CompressionImage(12);
             }
-            @lock.Exit();
+     //       @lock.Exit();
         }
         public MainWindow()
         {
@@ -69,24 +69,24 @@ namespace DrawAndGuess
 
             Plant.Source = BackSide;
 
-            CS = new Conversation();
+            Server = new Conversation();
             Loaded += (e, v) =>
             {
                 ImgMutil(true, (g) => { g.Clear(Color.FromArgb(255, 250, 250, 250)); }, ref PublicData);
 
-                CS.OnHosted += () =>
+                Server.OnHosted += () =>
                   {
                       Dispatcher.Invoke(() => { 
                           ImgMutil(true, (g) => { g.Clear(Color.FromArgb(255, 250, 250, 250)); }, ref PublicData);
                       });
                   };
-                CS.UnHosted += () => 
+                Server.UnHosted += () => 
                 {
                     Dispatcher.Invoke(() => {
                         ImgMutil(true, (g) => { g.Clear(Color.FromArgb(255, 250, 250, 250)); }, ref PublicData);
                     });
                 };
-                CS.OnBitmap += (b) =>
+                Server.OnBitmap += (b) =>
                 {
                     Dispatcher.Invoke(() => {
                         //   b.Save(times++.ToString() + ".png");
@@ -103,15 +103,22 @@ namespace DrawAndGuess
                 {
                     while (true)
                     {
-                        if (CS.IsMainHost)
+                        if (Server.IsMainHost)
                         {
                             byte[] Db = new byte[1];
                             Dispatcher.Invoke(() => {
                                 BackSide.Lock();
+                            });
+                            try
+                            {
                                 ImgMutil(false, null, ref Db);
+                            }catch (Exception ex)
+                            { }
+                            Dispatcher.Invoke(() => {
                                 BackSide.Unlock();
                             });
-                            CS.PublishImg(Db);
+                            Server.PublishImg(Db);
+                            Task.Delay(250);
                         }
                     }
                 });
@@ -128,21 +135,21 @@ namespace DrawAndGuess
             var FirstPen = e.GetStylusPoints(Plant)[0];
             var Point = new System.Drawing.Point((int)FirstPen.X, (int)FirstPen.Y);
             Save_Last.Clear();
-            Save_Last.AddRange(new Point[] { Point, Point, Point });
+            Save_Last.AddRange(new Point[] { Point, Point });
         }
         List<System.Drawing.Point> Save_Last = new List<System.Drawing.Point>();
         private void Window_StylusMove(object sender, StylusEventArgs e)
         {
-            if (!CS.IsMainHost) return;
             var FirstPen = e.GetStylusPoints(Plant)[0];
             var Point = new System.Drawing.Point((int)FirstPen.X, (int)FirstPen.Y);
+            Title = FirstPen.PressureFactor.ToString();
+            if (!Server.IsMainHost) return;
 
-            using (Pen pen = new Pen(Color.Black, 3.5F))
-                ImgMutil(true, (g) => { g.DrawCurve(pen, Save_Last.ToArray()); }, ref PublicData);
+            using (Pen pen = new Pen(Color.Black, 7f* FirstPen.PressureFactor))
+                ImgMutil(true, (g) => { g.DrawLines(pen, Save_Last.ToArray()); }, ref PublicData);
             Save_Last.RemoveAt(0);
 
             Save_Last.Add(Point);
-            Title = Point.ToString();
         }
         int times = 0;
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -151,11 +158,33 @@ namespace DrawAndGuess
             Id.IsEnabled = false;
             (sender as Button).IsEnabled = false;
 
-            CS.SetName(m_Id);
+            Server.SetName(m_Id);
             
         }
 
         string m_Id = "";
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+         //   var FirstPen = e.GetPosition(Plant);
+         //   var Point = new System.Drawing.Point((int)FirstPen.X, (int)FirstPen.Y);
+         //   Save_Last.Clear();
+         //   Save_Last.AddRange(new Point[] { Point, Point, Point });
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+        /*    if (!Server.IsMainHost || Mouse.LeftButton != MouseButtonState.Pressed) return;
+            var FirstPen = e.GetPosition(Plant);
+            var Point = new System.Drawing.Point((int)FirstPen.X, (int)FirstPen.Y);
+
+            using (Pen pen = new Pen(Color.Black, 3.5F))
+                ImgMutil(true, (g) => { g.DrawLines(pen, Save_Last.ToArray()); }, ref PublicData);
+            Save_Last.RemoveAt(0);
+
+            Save_Last.Add(Point);
+            Title = Point.ToString();*/
+        }
     }
 
 }
